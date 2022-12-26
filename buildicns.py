a2f22e25d442ed4912d7b73e5f33bcbc1c45188c
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2022-12-26 19:57:37 +0100

    b

--- buildicns.py
+++ buildicns.py
@@ -18,4 +18,10 @@ toolsPath = os.path.join(os.getcwd(), toolsLocation)
 icnsPackPath = os.path.join(toolsPath, "icnspack")
 icnsFilePath = os.path.join(exportIcnsPath, "Apple.icns")
 pngFilePath = os.path.join(exportPngPath, "Apple.png")
-subprocess.Popen(['sh', icnsPackPath, icnsFilePath, pngFilePath, pngFilePath])
+print("icnsPackPath: " + icnsPackPath)
+print("icnsFilePath: " + icnsFilePath)
+print("pngFilePath: " + pngFilePath)
+# subprocess on single line
+command = f"{icnsPackPath} {icnsFilePath} {pngFilePath} {pngFilePath}"
+print("command: " + command)
+subprocess.call(command, shell=True)
