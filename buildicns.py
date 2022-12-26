1c1479e89254b389ce96c9e3cf29d09c16d1eb7b
Stijn Rombouts <62154074+Core-i99@users.noreply.github.com>
2022-12-26 19:45:00 +0100

    Add workflow & remove snow pile

---
+++ buildicns.py
@@ -0,0 +1,22 @@
+import os
+import subprocess
+
+sourceLocation = "Source/png"
+sourcePath = os.path.join(os.getcwd(), sourceLocation)
+exportLocation = "Export"
+exportPngLocation = "png"
+exportIcnsLocation = "icns"
+exportPath = os.path.join(os.getcwd(), exportLocation)
+exportPngPath = os.path.join(exportPath, exportPngLocation)
+exportIcnsPath = os.path.join(exportPath, exportIcnsLocation)
+pngOrigLocation = "Source/pngOrig"
+pngOrigPath = os.path.join(os.getcwd(), pngOrigLocation)
+toolsLocation = "Source/Tools"
+toolsPath = os.path.join(os.getcwd(), toolsLocation)
+
+for imageFile in os.listdir(exportPngPath):
+    # call subprocess to run icnspack
+    icnsPackPath = os.path.join(toolsPath, "icnspack")
+    icnsFilePath = os.path.join(exportIcnsPath, imageFile.replace(".png", ".icns"))
+    pngFilePath = os.path.join(exportPngPath, imageFile)
+    subprocess.call([icnsPackPath, icnsFilePath, pngFilePath, pngFilePath])
